95f97b66471b78f4f88452cbf6ad7592c5ebba53
Tania Whiting <tanwhiting@aol.com>
2018-08-16 00:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // write your code below!
 function happyHolidays(){
+<<<<<<< HEAD
   return("Happy holidays!");
 }
 
 function happyHolidaysTo(name){
   return(`Happy holidays, ${name}!`);
+=======
+  return("Happy Holidays!");
+}
+
+function happyHolidaysTo(name){
+  return(`Happy Holidays, ${name}!`);
+>>>>>>> 9364032da7e4e2e9c4fdae0329f844acbfa61131
 }
 
 function happyHolidayTo(holiday, name){
